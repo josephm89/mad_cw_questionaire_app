709dcfd178bdcd2534e3bab63992a8aa5b041e20
josephm89 <joseph.meszaros89@yahoo.co.uk>
2023-04-17 15:19:18 +0100

    rename app

--- app/src/main/java/com/example/mad_cw/AboutActivity.java
+++ app/src/main/java/com/example/mad_cw/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.example.mad_cw;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Menu;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_upload) {
+            Intent intent = new Intent(AboutActivity.this, UploadActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (item.getItemId() == R.id.action_home) {
+            Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
