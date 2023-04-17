cc183d74ac49663958a9417da8b29de26c1b220a
josephm89 <joseph.meszaros89@yahoo.co.uk>
2023-04-17 10:55:47 +0100

    add functions to talk to db

---
+++ app/src/main/java/com/example/mad_cw/Topic.java
@@ -0,0 +1,11 @@
+package com.example.mad_cw;
+
+public class Topic {
+    private String name;
+
+    public Topic(String name) {
+        this.name = name;
+    }
+
+    // Add getter and setter for the name field
+}
--- app/src/main/java/com/example/mad_cw/UploadActivity.java
+++ app/src/main/java/com/example/mad_cw/UploadActivity.java
@@ -3,6 +3,12 @@ package com.example.mad_cw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.List;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -11,4 +17,24 @@ public class UploadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
     }
+
+    // read the topics from the database and fill the list
+    private void loadTopics() {
+        List<Topic> topics = getTopicsFromDatabase(); // Implement this method to read topics from the SQLite database
+        ArrayAdapter<Topic> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, topics);
+        ListView topicsList = findViewById(R.id.topics_list);
+        topicsList.setAdapter(adapter);
+    }
+
+    private void setupTopicsList() {
+        ListView topicsList = findViewById(R.id.topics_list);
+        topicsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Topic selectedTopic = (Topic) parent.getItemAtPosition(position);
+                loadTopicQuestions(selectedTopic); // Implement this method to load the questions for the selected topic
+            }
+        });
+    }
+
 }
