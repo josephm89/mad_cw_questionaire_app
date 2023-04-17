a74c1c9a554a4ced8ecd998fe958c2c7a5b5f059
josephm89 <joseph.meszaros89@yahoo.co.uk>
2023-04-17 10:02:57 +0100

    setup main view and add question class

--- app/src/main/java/com/example/mad_cw/MainActivity.java
+++ app/src/main/java/com/example/mad_cw/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.mad_cw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Get the questions from sqlite
+    private List<Question> getQuestionsFromDatabase() {
+        // TODO: Implement the method to read questions from the SQLite database
+        return new ArrayList<Question>();
+    }
+
+    // show the questions and answer options
+    private void displayQuestion(Question question) {
+        TextView questionText = findViewById(R.id.question_text);
+        Button answerButtonA = findViewById(R.id.answer_button_a);
+        Button answerButtonB = findViewById(R.id.answer_button_b);
+        Button answerButtonC = findViewById(R.id.answer_button_c);
+
+        questionText.setText(question.getQuestion());
+        answerButtonA.setText(question.getAnswerA());
+        answerButtonB.setText(question.getAnswerB());
+        answerButtonC.setText(question.getAnswerC());
+    }
+
+    private void setupAnswerButtons() {
+        Button answerButtonA = findViewById(R.id.answer_button_a);
+        Button answerButtonB = findViewById(R.id.answer_button_b);
+        Button answerButtonC = findViewById(R.id.answer_button_c);
+
+        View.OnClickListener answerButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button clickedButton = (Button) view;
+                String selectedAnswer = clickedButton.getText().toString();
+                checkAnswer(selectedAnswer);
+            }
+        };
+
+        answerButtonA.setOnClickListener(answerButtonClickListener);
+        answerButtonB.setOnClickListener(answerButtonClickListener);
+        answerButtonC.setOnClickListener(answerButtonClickListener);
+    }
+
+    private void checkAnswer(String selectedAnswer) {
+        Question currentQuestion = getCurrentQuestion(); // Implement this method to get the current question
+        String correctAnswer = currentQuestion.getCorrectAnswer();
+        if (selectedAnswer.equals(correctAnswer)) {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/mad_cw/Question.java
@@ -0,0 +1,61 @@
+package com.example.mad_cw;
+
+public class Question {
+    private String question;
+    private String answerA;
+    private String answerB;
+    private String answerC;
+    private String correctAnswer;
+
+    public Question(String question, String answerA, String answerB, String answerC, String correctAnswer) {
+        this.question = question;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.correctAnswer = correctAnswer;
+    }
+
+    // setters, getters
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+
+}
